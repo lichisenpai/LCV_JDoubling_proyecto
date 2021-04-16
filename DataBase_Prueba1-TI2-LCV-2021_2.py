adc3adbde4b366ebe8dfa5d335ce80be0cf3c377
lichisenpai <417liatitcv@gmail.com>
2021-04-15 21:16:30 -0500

    intento de la escritura el json

--- DataBase_Prueba1-TI2-LCV-2021_2.py
+++ DataBase_Prueba1-TI2-LCV-2021_2.py
@@ -1,3 +1,4 @@
+import numpy as np
 import json 
 
 Jota_0_5Hz = [
@@ -10,3 +11,32 @@ Jota_0_5Hz = [
     }
 
 ]
+
+def new_data (E, J, S_n, d, w):
+    dato_n = {}
+
+    dato_n['E'] = E 
+    dato_n['J'] = J
+    dato_n['S_n'] = S_n
+    dato_n['d'] = d
+    dato_n['w'] = w
+    return dato_n
+
+
+#Jota_0_5Hz.append(new_data())
+Error = np.linspace(0.1, 0.5, 5)
+
+jotas = np.linspace(0.5, 12.0, 5)
+
+W = np.linspace(0.3, 9.0, 5)
+
+s_n = np.linspace(0.5, 4.0, 5)
+
+D = np.linspace(0.5, 3, 5)
+
+for a, b, c, e, f in zip(Error, jotas, s_n, D, W):
+    new_entry = new_data(a, b, c, e, f)
+    Jota_0_5Hz.append(new_entry)
+
+
+print(Jota_0_5Hz)
