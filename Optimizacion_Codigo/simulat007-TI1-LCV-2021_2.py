e44a9591d83d9312c2f49689f9139096682927f3
lichisenpai <417liatitcv@gmail.com>
2021-04-19 14:22:13 -0500

    optimizar como escribir el archivo de texto con funciones bien definidas

--- Optimizacion_Codigo/simulat007-TI1-LCV-2021_2.py
+++ Optimizacion_Codigo/simulat007-TI1-LCV-2021_2.py
@@ -3,42 +3,40 @@ from nmrsim import Multiplet
 from nmrsim.plt import mplplot
 import matplotlib.pyplot as plt 
 
-# J: cte acoplamiento (Hz), v: frecuencia central (Hz), r: vecinos, i: integracion
+# J: cte acoplamiento (Hz), v: frecuencia central (Hz), r: vecinos, I: integracion
+def nmr_sim (v, I, J, r):
+    td = Multiplet(v , I, [(J, r)]) 
+    grafica = mplplot(td.peaklist(), points=1000)
+    return grafica
 
-class Simulat:
-    """
-    def __init__(self, J, v, r, i):
-        self.J = J
-        self.v = v 
-        self.r = r
-        self.i = i 
-    """
-    def Grafica (self, points, w, J, v, r, i):
-        self.points = points
-        self.linewidth = w
-        self.td = Multiplet(v, i, [(J, r)])
-        self.grafica = mplplot(self.td.peaklist(), points , w) #w es ancho de señal
-        
 
-    def GraficBonita (self):
-        plt.plot(self.grafica[0], self.grafica[1])
-        plt.xlim(1100,1300)
-        #plt.ylim(0, 0.055)
-        plt.xlabel("Frecuencia (Hz)")
-        plt.show()
-"""
-    def ArchivoTxt (self): 
-        intensidades = (grafica[1]) * 10000
-        no_datos = len(grafica[1])
-        intensidades1 = [] 
-"""
-simulacion1 = Simulat()
-simulacion1.Grafica(1000,5,7.1,1200.0,1,1)
-simulacion1.GraficBonita() 
+def archiv_txt ():
+    equis = nmr_sim(1200.0, 1, 7.0, 1) #para que me recupere los datos de la funcion nmr_sim
+    intensidades = (equis[1])
+    no_datos = len(equis[1])
+    intensidades1 = []
+    for i in range (len(intensidades)):
+        intensidades1.append(round(intensidades[i], 6))
 
-td = Multiplet(1200.0, 1, [(7.1, 1)])
-print(td.v)
-print(td.I)
-print(td.J)
+    #Datos en x 
+    Hz = (equis[0])
+    min_Hz = (Hz[0])
+    max_Hz = (Hz[no_datos - 1])
 
-grafica = mplplot(td.peaklist(), points = 1000, w= 5) #w es ancho de señal
+    oracion1 = f"Number of data points      : {no_datos} \n"
+    oracion2 = f"Chemical shift range (ppm) : 1 1\n"
+    oracion3 = f"Chemical shift range (Hz)  : {min_Hz} {max_Hz}\n"
+
+    #escritura del archivo para guardar los datos
+    f = open('multiplete7.slc','w')
+    f.write(oracion1)
+    f.write(oracion2)
+    f.write(oracion3)
+    
+    for i in range (len(intensidades1)):
+        f.write(str(intensidades1[i])+"\n")
+    
+    f.close()
+
+
+archiv_txt()
